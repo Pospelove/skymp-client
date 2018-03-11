3e2e8c9a06cdd34681631e465eaa9e8800f39e0b
pospelov <pospelovlm@yandex.ru>
2018-03-11 05:02:36 +0300

    remove messages

--- SkympClient/ClientLogic/ClientLogic.cpp
+++ SkympClient/ClientLogic/ClientLogic.cpp
@@ -691,7 +691,6 @@ class ClientLogic : public ci::IClientLogic
 
 			uint16_t playerid = ~0;
 			bsIn.Read(playerid);
-			ci::Chat::AddMessage(L"movement" + std::to_wstring(playerid));
 			ci::MovementData movData;
 			Deserialize(bsIn, movData);
 			uint8_t enabled;
@@ -819,7 +818,6 @@ class ClientLogic : public ci::IClientLogic
 			Deserialize(bsIn, look);
 			bsIn.Read(baseNpc);
 			bsIn.Read(id);
-			ci::Chat::AddMessage(std::to_wstring(id));
 			Deserialize(bsIn, movement);
 			{
 				ci::LookData _;
@@ -926,7 +924,6 @@ class ClientLogic : public ci::IClientLogic
 		{
 			uint16_t id = ~0;
 			bsIn.Read(id);
-			ci::Chat::AddMessage(L"destroy " + std::to_wstring(id));
 			auto it = players.find(id);
 			if (it != players.end())
 			{
