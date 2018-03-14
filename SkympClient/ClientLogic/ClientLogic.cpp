de3997ecdb844e52fc24bfe09b3dc2111d43afb0
pospelov <pospelovlm@yandex.ru>
2018-03-14 19:57:56 +0300

    1.0.28b fake fix

--- SkympClient/ClientLogic/ClientLogic.cpp
+++ SkympClient/ClientLogic/ClientLogic.cpp
@@ -29,6 +29,7 @@ class ClientLogic : public ci::IClientLogic
 	ci::LocalPlayer *const localPlayer = ci::LocalPlayer::GetSingleton();
 	ci::LookData ld;
 	std::map<uint16_t, ci::IActor *> players;
+	std::map<uint16_t, clock_t> ctorMoment;
 	std::set<uint16_t> werewolfs;
 	std::map<uint16_t, std::shared_ptr<ci::Text3D>> playerBubbles;
 	std::map<uint32_t, ci::Object *> objects;
@@ -711,6 +712,17 @@ class ClientLogic : public ci::IClientLogic
 					if (hostedPlayers.count(playerid) == 0)
 					{
 						player->ApplyMovementData(movData);
+						auto remPl = dynamic_cast<ci::RemotePlayer *>(player);
+						if (remPl && !remPl->IsSpawned())
+						{
+							if (clock() - ctorMoment[playerid] > 29000)
+							{
+								ci::Log("FATAL:ClientLogic Bad Spawn");
+								ci::SetTimer(2000, [] {
+									std::exit(0);
+								});
+							}
+						}
 					}
 				}
 				player->SetCell(localPlayer->GetCell());
@@ -890,6 +902,7 @@ class ClientLogic : public ci::IClientLogic
 			newPl->SetMark(std::to_string(id));
 			players[id] = newPl;
 			lastFurniture[id] = 0;
+			ctorMoment[id] = clock();
 
 			//uint32_t baseNpc = 0;
 			//bsIn.Read(baseNpc);
@@ -2035,6 +2048,19 @@ class ClientLogic : public ci::IClientLogic
 						if ((size_t)playerBubbles[playerID].get() == addr)
 							playerBubbles[playerID] = nullptr;
 					});
+					try {
+						auto rem = dynamic_cast<ci::RemotePlayer *>(players.at(playerID));
+						if (rem && rem->IsSpawned() == false)
+						{
+							if (clock() - ctorMoment[playerID] > 5000)
+							{
+								ci::Log("ERROR:ClientLogic 3dtext bad pos source");
+								playerBubbles[playerID].reset();
+							}
+						}
+					}
+					catch (...) {
+					}
 				}
 			}
 			break;
