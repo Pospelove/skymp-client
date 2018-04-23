b30d8a136d26a85b2ce386fce8d75674a76a53b6
pospelov <pospelovlm@yandex.ru>
2018-04-24 00:09:18 +0300

    ciScript.cpp decomposition

---
+++ SkympClient/CoreInterface/ciScriptAPI.h
@@ -0,0 +1,52 @@
+#pragma once
+// To be included into ciScript.cpp
+
+namespace api
+{
+	void write(const std::string &s)
+	{
+		ci::Chat::AddMessage(StringToWstring(s));
+	}
+
+	void on(const std::string &eventName, luabridge::LuaRef fn)
+	{
+		if (gContext == nullptr)
+			return;
+
+		if (fn.isFunction())
+		{
+			gContext->on[eventName] = [eventName, fn] {
+				try {
+					fn();
+				}
+				catch (std::exception &e) {
+					ErrorHandling::SendError("FATAL:Lua skymp.on('%s', ...) %s", eventName.data(), e.what());
+					std::exit(0);
+				}
+			};
+		}
+		else
+		{
+			gContext->on.erase(eventName);
+		}
+	}
+
+	namespace gui
+	{
+#define IMGUIDEF(type, luaFuncDeclaration, lambdaToReturn) type luaFuncDeclaration {if (gContext == nullptr) return type(); if (GetContextThread() == Thread::DirectX) return lambdaToReturn(); ErrorHandling::SendError("FATAL:Lua gui member called from non-DirectX thread"); std::exit(0); }
+
+		IMGUIDEF(bool, begin(std::string name, ImGuiWindowFlags flags), [&] {
+			bool p_open = true;
+			ImGui::Begin(name.data(), &p_open, flags);
+			return p_open;
+		});
+
+		IMGUIDEF(void, endWindow(), [&] {
+			ImGui::End();
+		});
+
+		IMGUIDEF(bool, button(std::string text), [&] {
+			return ImGui::Button(text.data());
+		});
+	}
+}
